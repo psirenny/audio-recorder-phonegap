e400fa0d507ec189f308fbe316648c831450fd26
psirenny <djtorres0@gmail.com>
2014-02-14 16:53:35 -0500

    fixes

--- index.js
+++ index.js
@@ -8,13 +8,32 @@ module.exports = function () {
     permission: function (callback) {
       callback(true);
     },
+    send: function (url, callback) {
+      var transfer = new FileTransfer();
+      var opts = new FileUploadOptions();
+      opts.chunkedMode = false;
+      opts.fileKey = 'audio';
+      opts.fileName = this.rec.uri.substr(this.rec.uri.lastIndexOf('/') + 1);
+      opts.mimeType = 'audio/' + this.rec.ext;
+      transfer.upload(this.rec.uri, url, function (data) {
+        var res = JSON.parse(data.response);
+        callback(null, res.url);
+      }, callback, opts);
+    },
     start: function (callback) {
-      var ext = window.device.platform === 'Android' ? '.amr' : '.wav'
-      var url = 'documents://' + uuid.v1() + ext;
-      this.rec = new Media(url);
-      this.rec.url = url;
-      this.rec.startRecord();
-      callback();
+      var self = this
+        , ext = window.device.platform === 'Android' ? 'amr' : 'wav'
+        , fileName = uuid.v1() + '.' + ext;
+
+      requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+        fileSystem.root.getFile(fileName, {create: true}, function (fileEntry) {
+          self.rec = new Media('documents://' + fileName);
+          self.rec.startRecord();
+          self.rec.ext = ext;
+          self.rec.uri = fileEntry.toURI();
+          callback();
+        }, callback);
+      }, callback);
     },
     stop: function (callback) {
       this.rec.stopRecord();
