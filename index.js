18a5f4d0adbf656be8c88c841e0a3b56642f4a7a
psirenny <djtorres0@gmail.com>
2015-01-06 22:23:54 -0500

    work with new audio-recorder

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
 var uuid = require('node-uuid');
 
-module.exports = function () {
-  return {
-    available: function (callback) {
-      callback(typeof Media !== 'undefined');
-    },
-    clear: function (callback) {
-      this.rec = null;
-      callback();
-    },
-    name: 'phonegap',
-    permission: function (callback) {
-      callback(true);
-    },
-    send: function (url, callback) {
-      var transfer = new FileTransfer();
-      var opts = new FileUploadOptions();
-      opts.chunkedMode = false;
-      opts.fileKey = 'audio';
-      opts.fileName = this.rec.uri.substr(this.rec.uri.lastIndexOf('/') + 1);
-      opts.mimeType = 'audio/' + this.rec.ext;
-      transfer.upload(this.rec.uri, url, function (data) {
-        var res = JSON.parse(data.response);
-        callback(null, res);
-      }, callback, opts);
-    },
-    start: function (callback) {
-      var self = this
-        , ext = window.device.platform === 'Android' ? 'amr' : 'wav'
-        , fileName = uuid.v1() + '.' + ext;
+function Strategy() {}
 
-      requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
-        fileSystem.root.getFile(fileName, {create: true}, function (fileEntry) {
-          self.rec = new Media('documents://' + fileName);
-          self.rec.startRecord();
-          self.rec.ext = ext;
-          self.rec.uri = fileEntry.toURI();
-          callback();
-        }, callback);
-      }, callback);
-    },
-    stop: function (callback) {
-      this.rec.stopRecord();
-      callback(null, this.rec.url);
-    }
-  };
+Strategy.prototype.available = function (callback) {
+  callback(null, typeof Media !== 'undefined');
+};
+
+Strategy.prototype.create = function () {
+  this.data = {};
+};
+
+Strategy.prototype.destroy = function () {
+  this.data = {};
+};
+
+Strategy.prototype.permission = function (callback) {
+  callback(null, true);
+};
+
+Strategy.prototype.start = function (callback) {
+  var self = this;
+  var type = window.device.platform === 'Android' ? 'amr' : 'wav';
+  var filename = uuid.v1() + '.' + type;
+
+  function onFileSystem(fs) {
+    fs.root.getFile(filename, {create: true}, onFile);
+  }
+
+  function onFile(entry) {
+    self.data.rec = new Media('documents://' + filename);
+    self.data.rec.startRecord();
+    self.data.rec.type = type;
+    self.data.rec.url = entry.toURL();
+    callback(null);
+  }
+
+  requestFileSystem(
+    LocalFileSystem.PERSISTENT,
+    0,
+    onFileSystem,
+    callback
+  );
+};
+
+Strategy.prototype.stop = function (callback) {
+  var rec = this.data.rec;
+  rec.stopRecord();
+  callback(null, {type: rec.type, url: rec.url});
+};
+
+module.exports = function () {
+  return new Strategy();
 };
